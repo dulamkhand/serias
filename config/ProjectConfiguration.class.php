8c77f1e092b3185f710869e3cabc758dcaa89204
handaa.1224@gmail.com <handaa.1224@gmail.com@979acccb-897d-00ee-68bf-2e218cdcb953>
2014-05-16 10:22:33 +0000

    work

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'c://wamp//www//symfony14//lib/autoload/sfCoreAutoload.class.php';
-//require_once dirname(__FILE__).'/../lib/vendor/symfony14/lib/autoload/sfCoreAutoload.class.php';
+//require_once '/home1/urinesse/public_html/girasole/symfony14/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
@@ -9,9 +9,14 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
       $this->enablePlugins('sfDoctrinePlugin', 'sfThumbnailPlugin');
+      
       sfConfig::set('sf_web_dir', 'C:\wamp\www\imdb\web');
       sfConfig::set('sf_upload_dir', 'C:\wamp\www\imdb\web\u');
       sfConfig::set('rich_text_fck_js_dir', 'C:\wamp\www\imdb\web\js');
+      
+      //sfConfig::set('sf_web_dir', '/home1/urinesse/public_html/girasole/imdb/web');
+      //sfConfig::set('sf_upload_dir', '/home1/urinesse/public_html/girasole/imdb/web/u');
+      //sfConfig::set('rich_text_fck_js_dir', '/home1/urinesse/public_html/girasole/imdb/web/js');
   }
 
 }
